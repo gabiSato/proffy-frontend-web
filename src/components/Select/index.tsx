edd62be1961bda99d3de01bd782cee35413c9772
Gabriela Sato <gabisato.ssb@gmail.com>
2020-08-12 00:57:26 -0300

    v1.0

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,32 @@
+import React, { SelectHTMLAttributes } from "react";
+
+import "./styles.css";
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  label: string;
+  name: string;
+  options: Array<{
+    label: string;
+    value: string;
+  }>;
+}
+
+const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
+  return (
+    <div className="select-block">
+      <label htmlFor={name}>{label}</label>
+      <select value="" id={name} {...rest}>
+        <option value="" disabled hidden>
+          Selecione uma opção
+        </option>
+        {options.map(({ value, label }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
